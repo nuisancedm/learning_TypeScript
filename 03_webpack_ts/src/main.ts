c9a6c9a09a3990507a72c47132c0d0b2126ca5c7
nuisancedm <85547644+nuisancedm@users.noreply.github.com>
2021-09-23 11:22:22 +0800

    Add files via upload

--- 03_webpack_ts/src/main.ts
+++ 03_webpack_ts/src/main.ts
@@ -1,8 +1,10 @@
 /*
  * @Date: 2021-09-22 16:18:29
  * @Editors: Mazhihui-Niko
- * @LastEditTime: 2021-09-23 10:18:35
+ * @LastEditTime: 2021-09-23 11:05:58
  * @FilePath: \ts\03_webpack_ts\src\main.ts
  */
-import './01_接口'
+// import './01_接口'
+// import './02_函数类型'
+import './03_类类型'
 document.write("haha")
